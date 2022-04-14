bef6c69b7514f90662a889de20236e40243cd141
dasuperangel <dasuperangel@gmail.com>
2022-04-14 17:38:40 +0300

    Добавьте файлы проекта.

---
+++ Модуль4/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+class MainClass
+{
+	public static void Main(string[] args)
+	//Задание 4.1.17
+	{
+		Console.WriteLine("Напишите свой любимый цвет на английском с маленькой буквы");
+
+		var color = Console.ReadLine();
+
+		if (color == "red")
+		{
+			Console.BackgroundColor = ConsoleColor.Red;
+			Console.ForegroundColor = ConsoleColor.Black;
+
+			Console.WriteLine("Your color is red!");
+		}
+
+		else if (color == "green")
+		{
+			Console.BackgroundColor = ConsoleColor.Green;
+			Console.ForegroundColor = ConsoleColor.Black;
+
+			Console.WriteLine("Your color is green!");
+		}
+		else
+		{
+			Console.BackgroundColor = ConsoleColor.Cyan;
+			Console.ForegroundColor = ConsoleColor.Black;
+
+			Console.WriteLine("Your color is cyan!");
+		}
+	}
+}
+
+
